68ee7f1585d2879d9955f51b6c69f676869cdca1
PaulyGlott <chris@schiffhauer.com>
2014-01-09 13:36:37 -0700

    Enabled quote-enclosing all fields of a CSV.

--- FlatBread/FlatBread/MainWindow.xaml.cs
+++ FlatBread/FlatBread/MainWindow.xaml.cs
@@ -1,28 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace FlatBread
+﻿namespace FlatBread
 {
+    using System.IO;
+    using System.Text;
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void FileBrowseBtnClick(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                DefaultExt = ".txt",
+                Filter = "Text documents (.txt, .csv)|*.txt;*.csv",
+                Multiselect = false
+            };
+
+            var result = dlg.ShowDialog();
+
+            if (result != true)
+            {
+                return;
+            }
+
+            var filename = string.Join(";", dlg.FileNames);
+            this.FilenameTbx.Text = filename;
+        }
+
+        private void QuoteEncloseBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            var inputFile = FilenameTbx.Text;
+            var stringBuilder = new StringBuilder();
+
+            foreach (var line in File.ReadAllLines(inputFile))
+            {
+                stringBuilder.AppendLine(string.Format("\"{0}\"", string.Join("\",\"", line.Split(','))));
+            }
+
+            File.WriteAllText(string.Format(@"{0}\Output.csv", Path.GetDirectoryName(inputFile)), stringBuilder.ToString());
         }
     }
 }
